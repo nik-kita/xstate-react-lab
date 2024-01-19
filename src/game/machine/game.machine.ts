cda84905e23db59a901b9a08e1918da1a9fcd0d8
nikita <nik.moiseienko@gmail.com>
2024-01-19 12:29:07 +0100

    fix game_machine logic: is_bottom guard instead of detect_bottom action

--- src/game/machine/game.machine.ts
+++ src/game/machine/game.machine.ts
@@ -1,9 +1,9 @@
 import { setup } from "xstate";
 import { GameMachineContext } from "./context.ts";
-import { detect_bottom } from "./detect_bottom.action.ts";
 import { GameEvent } from "./events.ts";
 import { generate_matrix } from "./generate_matrix.action.ts";
 import { is_available_space_for_tetromino } from "./is_available_space_for_tetromino.guard.ts";
+import { is_bottom } from "./is_bottom.guard.ts";
 import { move_tetromino } from "./move_tetromino.action.ts";
 import { place_tetromino } from "./place_tetromino.action.ts";
 import { reset_context } from "./reset_context.action.ts";
@@ -13,20 +13,21 @@ export const machine = setup({
   actions: {
     move_tetromino,
     generate_matrix,
-    detect_bottom,
     rm_full_lines,
     place_tetromino,
     reset_context,
   },
   guards: {
+    is_bottom,
     is_available_space_for_tetromino,
   },
   types: {
+    events: {} as GameEvent,
     context: {} as GameMachineContext,
   },
 }).createMachine(
   {
-    id: "game 4",
+    id: "game 5",
     context: {},
     initial: "Idle",
     states: {
@@ -81,29 +82,28 @@ export const machine = setup({
         },
       },
       Fly: {
-        entry: {
-          type: "detect_bottom",
-          params: ({ context }) => {
-            console.log("detecting bottom on enter");
-            return {
-              matrix: context.matrix,
-              tetromino: context.current_tetromino!,
-            };
+        always: {
+          guard: {
+            type: "is_bottom",
+            params: ({ context }) => {
+              return {
+                matrix: context.matrix,
+                tetromino: context.current_tetromino,
+              };
+            },
+          },
+          target: "Tetromino_creation",
+          actions: {
+            type: "rm_full_lines",
+            params: ({ context }) => {
+              console.log("meet bottom");
+              return {
+                matrix: context.matrix,
+              };
+            },
           },
         },
         on: {
-          MEET_BOTTOM: {
-            target: "Tetromino_creation",
-            actions: {
-              type: "rm_full_lines",
-              params: ({ context }) => {
-                console.log("meet bottom");
-                return {
-                  matrix: context.matrix,
-                };
-              },
-            },
-          },
           MOVE: {
             actions: [
               {
@@ -111,16 +111,6 @@ export const machine = setup({
                 params: ({ event }) => {
                   return {
                     direction: event.direction,
-                  };
-                },
-              },
-              {
-                type: "detect_bottom",
-                params: ({ context }) => {
-                  console.log("detecting bottom on move");
-                  return {
-                    matrix: context.matrix,
-                    tetromino: context.current_tetromino!,
                   };
                 },
               },
@@ -139,9 +129,6 @@ export const machine = setup({
           },
         },
       },
-    },
-    types: {
-      events: {} as GameEvent,
     },
   },
 );
--- src/game/machine/is_bottom.guard.ts
+++ src/game/machine/is_bottom.guard.ts
@@ -1,18 +1,21 @@
-import { raise } from "xstate";
 import { Matrix } from "../engine/matrix.ts";
 import { Tetromino } from "../engine/tetromino.ts";
 import { GameMachineContext } from "./context.ts";
 
-export const detect_bottom = (_: { context: GameMachineContext }, params: {
+export const is_bottom = (_: { context: GameMachineContext }, params: {
   matrix?: Matrix;
   tetromino?: Tetromino;
 }) => {
   if (!params.tetromino || !params.matrix) {
     console.warn("<detect_bottom> was called unnecessary");
-    return;
+    return false;
   }
   if (params.matrix.detect_bottom().includes(params.tetromino._id)) {
     params.matrix.tetromino_to_bottom(params.tetromino._id);
-    raise({ type: "MEET_BOTTOM" });
+    console.log("bottom!");
+
+    return true;
   }
+
+  return false;
 };
