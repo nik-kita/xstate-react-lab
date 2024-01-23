7893150e0f1e14d03f9175985b61b21a84660e05
nikita <nik.moiseienko@gmail.com>
2024-01-23 15:40:43 +0100

    generate matrix

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,7 +1,20 @@
+import { GameContext } from "./GameContext";
+import { GameEventGenerator } from "./game/machine/events";
+
 export default function Game() {
+  const m = GameContext.useSelector((snapshot) => snapshot.context.matrix);
+  const actor_ref = GameContext.useActorRef();
   return (
     <div>
       <h1>Game</h1>
+      <pre>{JSON.stringify(m, null, 1)}</pre>
+      <button
+        onClick={() => {
+          actor_ref.send(GameEventGenerator.GENERATE_MATRIX(2, 4));
+        }}
+      >
+        generate matrix
+      </button>
     </div>
   );
 }
